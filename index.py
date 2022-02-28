92c961fd1d8c9d571e687da623b322ebcbc97e96
Christian Agano <98008450+agaknows@users.noreply.github.com>
2022-02-28 14:21:42 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -1,85 +1,93 @@
 from flask import Flask, request, jsonify
+import numpy as np
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 import requests
 #api_url = "https://deltime-regressionv1.herokuapp.com/"
 #response = requests.get(url, headers=header)
 
 app = Flask(__name__)
 
-
 delivery = pd.read_csv("deliverytime.csv")
-
 deltime = delivery['deltime']
 ncases = delivery['ncases']
 distance = delivery['distance']
 
-type(deltime)
 
-def corrfunction(vector1, vector2):
+def lrfunction(vector1, vector2):
     
-    v1mean = vector1.mean()
-    v1std = vector1.std()
-    v1=[]
+    delivery = pd.read_csv("deliverytime.csv")
+    deltime = delivery['deltime']
+    ncases = delivery['ncases']
+    distance = delivery['distance']
     
-    for i in vector1:
-        v1.append((i-v1mean)/v1std)
-        
-    v2mean = vector2.mean()
-    v2std = vector2.std()
-    v2 = []
+    x=delivery[['ncases','distance']]
+    y=delivery['deltime']
     
-    for i in vector2:
-        v2.append((i-v2mean)/v2std)
-        
+    regressor = LinearRegression()
+    regressor.fit(x,y)
     
-    df =pd.DataFrame([v1,v2])
-    df_t=df.T
+    b0 = regressor.intercept_
+    b1 = regressor.coef_[0]
+    b2 = regressor.coef_[1]
     
+    userinput1=pd.Series(float(input("Please enter ncases: ")))
+    userinput2=pd.Series(float(input("Please enter distance: ")))
+    userinput3=pd.Series(float(input("Please enter delivery time: ")))
     
-    df_t['Product']= (df_t[0]*df_t[1])
-    
-    reg = (df_t['Product'].sum())/(len(df_t)-1)
-    return(reg)
+    finaldeltime = str( (b1*userinput1) + (b2*userinput2) + b0)
+    return "deltime =" + finaldeltime
 
-def corrfunction2(vector1, vector2):
+
     
-    v1mean = vector1.mean()
-    v1std = vector1.std()
-    v1=[]
+
+def lrfunction2(vector1, vector2):
+    
+    
+    delivery = pd.read_csv("deliverytime.csv")
+    deltime = delivery['deltime']
+    ncases = delivery['ncases']
+    distance = delivery['distance']
     
     userinput1=pd.Series(float(input("Please enter ncases: "))) #ncases input
-    vector1 = vector1.append(userinput1)
-    for i in vector1:
-        v1.append((i-v1mean)/v1std)
-    
-        
-    v2mean = vector2.mean()
-    v2std = vector2.std()
-    v2 = []
-    
+    ncases = ncases.append(userinput1)
+    ncases = pd.DataFrame(ncases)
+
     userinput2=pd.Series(float(input("Please enter distance: "))) #distance input
-    vector2 = vector2.append(userinput2)
-    for i in vector2:
-        v2.append((i-v2mean)/v2std)
+    distance = distance.append(userinput2)
+    distance = pd.DataFrame(distance)
     
     userinput3=pd.Series(float(input("Please enter delivery time: "))) #deltime input
+    deltime = deltime.append(userinput3)
+    deltime = pd.DataFrame(deltime)
     
-    df =pd.DataFrame([v1,v2])
-    df_t=df.T
+    delll = pd.concat([ncases, distance, deltime], axis = 1, ignore_index=True)
+    delll.columns = ['ncases','distance','deltime']
     
-    print(df_t)
+    print(delll)
     
-    df_t['Product']= (df_t[0]*df_t[1])
+    x=delll[['ncases','distance']]
+    y=delll['deltime']
     
-    reg = (df_t['Product'].sum())/(len(df_t)-1)
-    return(reg)
+    regressor = LinearRegression()
+    regressor.fit(x,y)
+    
+    b0 = regressor.intercept_
+    b1 = regressor.coef_[0]
+    b2 = regressor.coef_[1]
+    
+    return "deltime = " + str(b1) + "(ncases) + " + str(b2) + "(distance) + " + str(b0)
+
+
+
 
 @app.get("/regression")
 def get_reg():
     headers = request.headers
     auth = headers.get("api-key")
     if auth == "mypassword":
-        return jsonify (corrfunction(ncases, distance))
+        return jsonify (lrfunction(ncases, distance))
     else:
         return jsonify({"message":"Error: unauthorized"}),401
     
@@ -88,7 +96,7 @@ def get_reg2():
     headers = request.headers
     auth = headers.get("api-key")
     if auth == "mypassword":
-        return jsonify (corrfunction2(ncases, distance))
+        return jsonify (lrfunction2(ncases, distance))
     else:
         return jsonify({"message":"Error: unauthorized"}),401
 
@@ -101,4 +109,7 @@ api_url = "http://127.0.0.1:5000/regression"
 headers={'Content-type': 'application/json', 'Accept': 'text/plain', 'api-key': 'mypassword'}
 
 response=requests.get(api_url,headers=header)
+print(response.json())
+
+response=requests.post(api_url,headers=header)
 print(response.json())
